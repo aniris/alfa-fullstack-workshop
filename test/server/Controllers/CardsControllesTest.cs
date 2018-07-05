0e58921d9113fa50872e8bbae421dfece285d427
Irina Soloveva <aniri.solovyeva@gmail.com>
2018-07-05 03:30:27 +0300

    add tests for controllers

--- test/server/Controllers/CardsControllesTest.cs
+++ test/server/Controllers/CardsControllesTest.cs
@@ -1,9 +1,11 @@
+using System;
 using Server.Controllers;
 using Server.Data;
 using Xunit;
 using Moq;
 using System.Linq;
 using System.Collections.Generic;
+using Server.Infrastructure;
 using Server.Models;
 using Server.Services;
 
@@ -31,6 +33,74 @@ namespace ServerTest.ControllersTest
             // Assert
             mock.Verify(r => r.GetCards(), Times.AtMostOnce());
             Assert.Equal(3, cards.Count());
+        }
+        
+        [Fact]
+        public void GetCardByNumberPassed()
+        {
+            // Arrange
+            var mock = new Mock<IBankRepository>();
+            var mockUser = FakeDataGenerator.GenerateFakeUser();
+            var mockCards = FakeDataGenerator.GenerateFakeCardsToUser(mockUser);
+
+            var numberFirstCard = mockCards.First().CardNumber;
+            
+            mock.Setup(r => r.GetCard(numberFirstCard)).Returns(mockCards.First());
+
+            var controller = new CardsController(mock.Object, _cardService);
+
+            // Test
+            var card = controller.Get(numberFirstCard);
+
+            // Assert
+            mock.Verify(r => r.GetCard(numberFirstCard), Times.AtMostOnce());
+            Assert.Equal(numberFirstCard, card.CardNumber);
+        }
+        
+        [Fact]
+        public void GetCardByNumberFailed()
+        {
+            // Arrange
+            var mock = new Mock<IBankRepository>();
+            var mockUser = FakeDataGenerator.GenerateFakeUser();
+            var mockCards = FakeDataGenerator.GenerateFakeCardsToUser(mockUser);
+            var mockTransactions = FakeDataGenerator.GenerateFakeTransactionstoCard(mockCards.First());
+            var numberFirstCard = mockCards.First().CardNumber;
+            
+            mock.Setup(r => r.GetCard(numberFirstCard)).Returns(mockCards.First());
+
+            var controller = new CardsController(mock.Object, _cardService);
+
+            // Test
+            var card = controller.Get(numberFirstCard);
+
+            // Assert
+            mock.Verify(r => r.GetCard(numberFirstCard), Times.AtMostOnce());
+            Assert.NotEqual("1234567812345678", card.CardNumber);
+        }
+        
+        [Fact]
+        public void DeleteCardFailed()
+        {
+            // Arrange
+            var mock = new Mock<IBankRepository>();
+            var mockUser = FakeDataGenerator.GenerateFakeUser();
+            var mockCards = FakeDataGenerator.GenerateFakeCardsToUser(mockUser);
+
+            var controller = new TransactionsController(mock.Object, _cardService);
+            bool exceptionCatch = false;
+
+            // Test
+            try
+            {
+                var transactions = controller.Delete(mockCards.First().CardNumber);
+            }
+            catch (Exception)
+            {
+                exceptionCatch = true;
+            }
+            
+            Assert.True(exceptionCatch);
         }
     }
 }
---
+++ test/server/Controllers/TransactionControllerTest.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Newtonsoft.Json.Linq;
+using Server.Controllers;
+using Server.Data;
+using Server.Infrastructure;
+using Server.Services;
+using Xunit;
+
+namespace ServerTest.ControllersTest
+{
+    public class TransactionControllerTest
+    {
+        private readonly ICardService _cardService = new CardService();
+        
+        [Fact]
+        public void GetTransactionsPassed()
+        {
+            // Arrange
+            var mock = new Mock<IBankRepository>();
+            var mockUser = FakeDataGenerator.GenerateFakeUser();
+            var mockCards = FakeDataGenerator.GenerateFakeCardsToUser(mockUser);
+            var mockTransactions = FakeDataGenerator.GenerateFakeTransactionstoCard(mockCards.First());
+
+            mock.Setup(r => r.GetTranasctions(mockCards.First().CardNumber, 1)).Returns(mockTransactions.Skip(1).Take(10));
+
+            var controller = new TransactionsController(mock.Object, _cardService);
+
+            // Test
+            var transactions = controller.Get(1, mockCards.First().CardNumber);
+
+            // Assert
+            mock.Verify(r => r.GetTranasctions(mockCards.First().CardNumber, 1), Times.AtMostOnce());
+            Assert.Equal(10, transactions.Count());
+        }
+        
+        [Fact]
+        public void GetTransactionsFailed()
+        {
+            // Arrange
+            var mock = new Mock<IBankRepository>();
+            var mockUser = FakeDataGenerator.GenerateFakeUser();
+            var mockCards = FakeDataGenerator.GenerateFakeCardsToUser(mockUser);
+            var mockTransactions = FakeDataGenerator.GenerateFakeTransactionstoCard(mockCards.First());
+
+            mock.Setup(r => r.GetTranasctions(mockCards.First().CardNumber, 1)).Returns(mockTransactions.Skip(1).Take(10));
+
+            var controller = new TransactionsController(mock.Object, _cardService);
+
+            // Test
+            var transactions = controller.Get(1, mockCards.First().CardNumber);
+
+            // Assert
+            mock.Verify(r => r.GetTranasctions(mockCards.First().CardNumber, 1), Times.AtMostOnce());
+            Assert.NotEqual(0, transactions.Count());
+        }
+        
+        [Fact]
+        public void DeleteTransactionFailed()
+        {
+            // Arrange
+            var mock = new Mock<IBankRepository>();
+            var mockUser = FakeDataGenerator.GenerateFakeUser();
+            var mockCards = FakeDataGenerator.GenerateFakeCardsToUser(mockUser);
+
+            var controller = new TransactionsController(mock.Object, _cardService);
+            bool exceptionCatch = false;
+
+            // Test
+            try
+            {
+                var transactions = controller.Delete(mockCards.First().CardNumber);
+            }
+            catch (Exception)
+            {
+                exceptionCatch = true;
+            }
+            
+            Assert.True(exceptionCatch);
+        }
+    }
+}
