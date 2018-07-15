5b0ca58494fed24588f98220b06546ca6585dcda
Irina Soloveva <aniri.solovyeva@gmail.com>
2018-07-15 03:42:55 +0300

    adds processing of transfer money

--- src/client/src/actions/payments.js
+++ src/client/src/actions/payments.js
@@ -1,4 +1,8 @@
 import * as action from "./types";
+import axios from "axios";
+import { fetchTransactions } from "./transactions";
+
+const ROOT_URL = "/api";
 
 /**
  * Проводит withdraw транзакцию
@@ -9,7 +13,33 @@ import * as action from "./types";
  * @returns
  */
 export const TransferMoney = (from, to, sum) => {
-  //TODO
+  return async dispatch => {
+    try {
+      dispatch({
+        type: action.PAYMENT_STARTED
+      });
+
+      const response = await axios.post(`${ROOT_URL}/transactions`, { sum, from, to });
+
+      dispatch({
+        type: action.PAYMENT_SUCCESS,
+        payload: response.data
+      });
+
+      dispatch(fetchTransactions(response.data.from))
+    }
+    catch (err) {
+      console.log(err);
+
+      dispatch({
+        type: action.PAYMENT_FAILED,
+        payload: err.response
+      });
+      console.log(
+        err.response
+      );
+    }
+  }
 };
 
 export const repeateTransferMoney = () => dispatch =>
